3a46e7a782db0f0512ef55e7334f98e3a0d1e302
Tac Tacelosky <tacman@gmail.com>
2025-05-26 08:09:29 -0400

    remove polyfill, bump to 7.3rc1

--- src/Kernel.php
+++ src/Kernel.php
@@ -8,4 +8,12 @@ use Symfony\Component\HttpKernel\Kernel as BaseKernel;
 class Kernel extends BaseKernel
 {
     use MicroKernelTrait;
+
+    public function getCacheDir(): string
+    {
+        if (isset($_SERVER['HOME'])) {
+            return $_SERVER['HOME'] . '/.myapp/cache/' . $this->environment;
+        }
+        return sys_get_temp_dir() . '/myapp_cache/' . $this->environment;
+    }
 }
