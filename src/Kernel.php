79a749499d9da89e3e95a6946f517e6a16e23d81
Tac Tacelosky <tacman@gmail.com>
2025-05-26 08:53:06 -0400

    hack isRunningFromPhar

--- src/Kernel.php
+++ src/Kernel.php
@@ -11,9 +11,18 @@ class Kernel extends BaseKernel
 
     public function getCacheDir(): string
     {
+        if ($this->isRunningFromPhar()) {
+//            die("Stopped, running phar is already running");
+        }
         if (isset($_SERVER['HOME'])) {
             return $_SERVER['HOME'] . '/.myapp/cache/' . $this->environment;
         }
         return sys_get_temp_dir() . '/myapp_cache/' . $this->environment;
     }
+
+    private function isRunningFromPhar(): bool
+    {
+        return strpos(__FILE__, 'phar://') === 0;
+    }
+
 }
