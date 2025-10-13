ab0d9d136c41bcfd75f1d7bb958a6a65e5d7b26d
Théo FIDRY <theo.fidry@gmail.com>
2025-10-13 19:45:22 +0200

    make it work

--- src/Kernel.php
+++ src/Kernel.php
@@ -9,15 +9,9 @@ class Kernel extends BaseKernel
 {
     use MicroKernelTrait;
 
-    public function getCacheDir(): string
+    public function getProjectDir(): string
     {
-        if ($this->isRunningFromPhar()) {
-//            die("Stopped, running phar is already running");
-        }
-        if (isset($_SERVER['HOME'])) {
-            return $_SERVER['HOME'] . '/.myapp/cache/' . $this->environment;
-        }
-        return sys_get_temp_dir() . '/myapp_cache/' . $this->environment;
+        return __DIR__.'/../';
     }
 
     private function isRunningFromPhar(): bool
